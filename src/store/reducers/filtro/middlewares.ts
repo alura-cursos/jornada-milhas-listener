dac421d4dc6e4b05957ba169e971fa61e5c45f90
Luiz Fernando <lfrprazeres@gmail.com>
2023-11-02 17:53:26 -0300

    5.2 - Snackbar cancelar query

--- src/store/reducers/filtro/middlewares.ts
+++ src/store/reducers/filtro/middlewares.ts
@@ -1,9 +1,9 @@
-import { createListenerMiddleware } from '@reduxjs/toolkit';
-import { baseUrl } from 'src/config/api';
-import { mudarFiltro } from './index';
-import { endpoints, util } from '../viagem/middlewares';
-import { criarSnackbar, deletarSnackbar } from '../snackbar';
-import uuid from 'react-native-uuid';
+import { createListenerMiddleware } from "@reduxjs/toolkit";
+import { baseUrl } from "src/config/api";
+import { mudarFiltro } from "./index";
+import { endpoints, util } from "../viagem/middlewares";
+import { criarSnackbar, deletarSnackbar } from "../snackbar";
+import uuid from "react-native-uuid";
 
 export const filtroListener = createListenerMiddleware();
 
@@ -12,16 +12,18 @@ filtroListener.startListening({
   effect: async (action, api) => {
     const [origens, destinos] = await Promise.all([
       fetch(`${baseUrl}origens`),
-      (await fetch(`${baseUrl}destinos`)).json()
+      (await fetch(`${baseUrl}destinos`)).json(),
     ]);
 
-    api.dispatch(mudarFiltro({
-      origens: await origens.json(),
-      destinos
-    }));
+    api.dispatch(
+      mudarFiltro({
+        origens: await origens.json(),
+        destinos,
+      })
+    );
 
     api.unsubscribe();
-  }
+  },
 });
 
 filtroListener.startListening({
@@ -29,31 +31,37 @@ filtroListener.startListening({
   effect: async (action, api) => {
     const quantidadeViagens = action.payload.viagens.length;
 
-    const textoViagem = `viage${quantidadeViagens > 1 ? 'ns' : 'm'}`;
-    const textoBuscado = `buscada${quantidadeViagens > 1 ? 's' : ''}`;
+    const textoViagem = `viage${quantidadeViagens > 1 ? "ns" : "m"}`;
+    const textoBuscado = `buscada${quantidadeViagens > 1 ? "s" : ""}`;
 
     if (quantidadeViagens >= 1) {
-      api.dispatch(criarSnackbar({
-        mensagem: `${quantidadeViagens} ${textoViagem} ${textoBuscado} com sucesso!`
-      }));
+      api.dispatch(
+        criarSnackbar({
+          mensagem: `${quantidadeViagens} ${textoViagem} ${textoBuscado} com sucesso!`,
+        })
+      );
     } else {
-      api.dispatch(criarSnackbar({
-        mensagem: 'Nenhuma viagem encontrada',
-        tipo: 'warning',
-      }))
+      api.dispatch(
+        criarSnackbar({
+          mensagem: "Nenhuma viagem encontrada",
+          tipo: "warning",
+        })
+      );
     }
-  }
+  },
 });
 
 filtroListener.startListening({
   matcher: endpoints.getViagens.matchPending,
   effect: async (action, api) => {
     const id = String(uuid.v4());
-    api.dispatch(criarSnackbar({
-      mensagem: 'buscando viagem',
-      tipo: 'info',
-      id
-    }));
+    api.dispatch(
+      criarSnackbar({
+        mensagem: "buscando viagem",
+        tipo: "info",
+        id,
+      })
+    );
 
     api.fork(async (forkApi) => {
       await forkApi.delay(2000);
@@ -62,6 +70,14 @@ filtroListener.startListening({
 
     const queries = api.dispatch(util.getRunningQueriesThunk());
 
-    queries.map(query => query.abort());
-  }
-})
+    queries.map((query) => {
+      api.dispatch(
+        criarSnackbar({
+          mensagem: "cancelando query",
+          tipo: "warning",
+        })
+      );
+      query.abort();
+    });
+  },
+});
