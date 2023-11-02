a840e5be41be5073f9bf21dba4ca5d55d3d55bc8
Luiz Fernando <lfrprazeres@gmail.com>
2023-11-02 16:57:45 -0300

    3.3 - Criando snackbar

--- src/store/reducers/filtro/middlewares.ts
+++ src/store/reducers/filtro/middlewares.ts
@@ -2,6 +2,7 @@ import { createListenerMiddleware } from '@reduxjs/toolkit';
 import { baseUrl } from 'src/config/api';
 import { mudarFiltro } from './index';
 import { endpoints } from '../viagem/middlewares';
+import { criarSnackbar } from '../snackbar';
 
 export const filtroListener = createListenerMiddleware();
 
@@ -16,6 +17,12 @@ filtroListener.startListening({
     api.dispatch(mudarFiltro({
       origens: await origens.json(),
       destinos
+    }));
+    
+    const quantidadeViagens = action.payload.viagens.length;
+
+    api.dispatch(criarSnackbar({
+      mensagem: `${quantidadeViagens} viage${quantidadeViagens > 1 ? 'ns' : 'm'} buscada com sucesso!`
     }));
 
     api.unsubscribe();
