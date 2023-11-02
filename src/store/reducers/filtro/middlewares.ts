8d9e7b598caef352d46d43c122bc5d5332b2d983
Luiz Fernando <lfrprazeres@gmail.com>
2023-11-02 16:31:28 -0300

    2.3 - Buscando origens e destinos

--- src/store/reducers/filtro/middlewares.ts
+++ src/store/reducers/filtro/middlewares.ts
@@ -1,10 +1,18 @@
 import { createListenerMiddleware } from '@reduxjs/toolkit';
+import { baseUrl } from 'src/config/api';
+import { mudarFiltro } from './index';
 
 export const filtroListener = createListenerMiddleware();
 
 filtroListener.startListening({
   type: 'viagensApi/executeQuery/fulfilled',
-  effect: async () => {
-    console.log('escutando!');
+  effect: async (action, api) => {
+    const origens = await (await fetch(`${baseUrl}origens`)).json();
+    const destinos = await (await fetch(`${baseUrl}destinos`)).json();
+
+    api.dispatch(mudarFiltro({
+      origens,
+      destinos
+    }))
   }
 })
