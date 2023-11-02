alura-cursos/jornada-milhas-redux

código inicial

src/types/viagem.ts
@@ -1,0 +1,17 @@
+import { ImageSourcePropType } from 'react-native';
+
+export enum TipoViagem {
+  ida = 'ida',
+  idaEVolta = 'idaEVolta'
+}; 
+export interface Viagem {
+  titulo: string,
+  valor: number,
+  foto: ImageSourcePropType,
+  dataIda: string,
+  dataVolta?: string,
+  origem: string,
+  estadoOrigem: string,
+  destino: string,
+  tipo: TipoViagem
+}

alura-cursos/jornada-milhas-listener

src/types/viagem.ts
@@ -1,0 +1,15 @@
+export enum TipoViagem {
+  ida = 'ida',
+  idaEVolta = 'idaEVolta'
+}; 
+export interface Viagem {
+  titulo: string,
+  valor: number,
+  foto: string,
+  dataIda: string,
+  dataVolta?: string,
+  origem: string,
+  estadoOrigem: string,
+  destino: string,
+  tipo: TipoViagem
+}